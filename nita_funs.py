30e7cf57d67883d570ce73820a0c1d545d271f93
Leyang Feng <feng@american.edu>
2018-06-04 17:27:43 -0400

    Another playground

--- nita_funs.py
+++ nita_funs.py
@@ -1,6 +1,205 @@
 #%%
 import numpy as np
-import utility_funs as uf
+import pandas as pd
+import math
+from scipy import stats  
+import matplotlib.pyplot as plt
+
+#%%
+def filterLimits(x, y, doy_vec, value_limits, date_limits, doy_limits):
+   
+    # ---
+    # (1)
+    if sum(date_limits) == 0:
+        x = x
+        y = y
+        doy_vec = doy_vec
+    else:
+        if date_limits[0] == -9999 & date_limits[1] != -9999:
+            start_date = x[0]
+            end_date = date_limits[1]
+        elif date_limits[0] != -9999 & date_limits[1] == 9999:
+            start_date = date_limits[0]
+            end_date = x[-1]
+        else:
+            start_date = date_limits[0]
+            end_date = date_limits[-1]
+    
+        date_flags = (x >= start_date) & (x <= end_date)
+        x = x[date_flags]
+        y = y[date_flags]
+        doy_vec = doy_vec[date_flags]
+    
+    # ---
+    # (2)
+    doy_flags = np.full(doy_vec.shape, False, dtype=bool)
+    for doy_limit in doy_limits:
+        doy_flag_it = (doy_vec >= doy_limit[0]) & (doy_vec <= doy_limit[1])
+        doy_flags = doy_flags | doy_flag_it
+    
+    x = x[doy_flags]
+    y = y[doy_flags]
+    doy_vec = doy_vec[doy_flags]
+    
+    # ---
+    # (3)
+    non_nan_flags = ~np.isnan(y)
+    x = x[non_nan_flags]
+    y = y[non_nan_flags]
+    doy_vec = doy_vec[non_nan_flags]
+    
+    # ---
+    # (4)
+    value_flags = np.logical_and(y >= value_limits[0], y <= value_limits[1]) 
+    x = x[value_flags]
+    y = y[value_flags]
+    doy_vec = doy_vec[value_flags]
+    
+    return x, y, doy_vec
+
+#%%
+def distancePointEdge(points, edge):
+    # Python implementation of MATLAB function distancePointEdge created by David Legland 
+    
+    # edge shoud be in the format of [x0, y0, x1, y1]
+    # pts should be in the format of [[x1, y1],
+    #                                 [x2, y2],
+    #                                   ...   
+    #                                 [xn,yn]]
+    
+    edge = np.array(edge).reshape((4,1))
+    points = np.array(points) 
+    
+    if edge.shape != (4,1):
+        raise ValueError('in-valid edge shape!')
+     
+    if points.shape[1] != 2:
+        raise ValueError('in-valid points shape!')
+        
+    # direction vector of each edge
+    dx = edge[2] - edge[0]
+    dy = edge[3] - edge[1]
+
+    # compute position of points projected on the supporting line
+    # (Size of tp is the max number of edges or points)   
+    delta = dx * dx + dy * dy
+    tp = ((points[:, 0] - edge[0]) * dx + (points[:, 1] - edge[1]) *dy) / delta
+
+
+    # change position to ensure projected point is located on the edge
+    tp[tp < 0] = 0;
+    tp[tp > 1] = 1;
+
+    # coordinates of projected point
+    p0 = np.column_stack((edge[0] + tp * dx, edge[1] + tp * dy))
+
+    # compute distance between point and its projection on the edge
+    dist = np.sqrt((points[:,0] - p0[:,0]) ** 2 + (points[:,1] - p0[:,1]) ** 2);
+
+    return dist 
+
+#%%
+def calDistance(knot_set, coeff_set, pts):
+    
+    pts = np.array(pts) 
+    
+    dist_mat = np.empty((pts.shape[0],0))
+    for i in range(0,len(knot_set)-1):
+        edge = [knot_set[i], coeff_set[i], knot_set[i+1], coeff_set[i+1]]
+        dist_mat_i = distancePointEdge(pts, edge)
+        dist_mat = np.column_stack((dist_mat, dist_mat_i))
+    
+    return dist_mat
+
+#%%
+def calMae(dist):
+    dist = dist.min(axis=1)
+    mae = dist.mean()
+    return mae
+
+#%%
+def findCandidate(dist, filt_dist, pct, 
+                  y, loc_set, filter_opt='movcv'):
+    
+    dist = dist.min(axis=1)
+    
+    #mov_mean = pd.Series(list(dist)).rolling(window=filt_dist, closed='both').mean()    
+    
+    mov_mean = pd.rolling_mean(dist, filt_dist, min_periods=1)
+    mov_std = pd.rolling_std(dist, filt_dist, min_periods=1)
+    
+    if filter_opt == 'movcv':
+        search_series = (mov_mean / mov_std)
+        
+    invalid_ss_idx = list(set(list(range(0,filt_dist)) + 
+                              list(range(len(search_series) - filt_dist, len(search_series))) + 
+                              list(loc_set)))
+    search_series_inner = np.delete(search_series, invalid_ss_idx, None) # the N-1 search_series got flatten in here
+    
+    if len(search_series_inner) == 0:
+        cand_loc = -999
+        coeff = -999 
+    else:
+        cand_loc = int(np.where(search_series.flatten() == search_series_inner.max())[0])
+        cand_loc_filt = list(range(int(cand_loc - ((filt_dist - 1) / 2)), int(cand_loc + ((filt_dist - 1) / 2) + 1)))
+        coeff = np.percentile(y[cand_loc_filt], pct, interpolation='midpoint')
+        
+    return cand_loc, coeff
+
+#%%
+def updateknotcoeffSet(knot_set, coeff_set, loc_set, x, cand_loc, coeff):
+    knot_val = x[cand_loc]
+    knot_set = np.unique(np.append(knot_set, knot_val))
+    new_knot_loc = int(np.where(knot_set == knot_val)[0])
+    coeff_set = np.insert(coeff_set, new_knot_loc, coeff)
+    loc_set = np.unique(np.append(loc_set, cand_loc))
+
+    return knot_set, coeff_set, loc_set 
+
+#%%
+def genKeepIdx(keep_knots, keep_coeffs, pts, pct, y_pos_flags):
+    if len(keep_knots) - 2 > 0:
+        mae_iter_ortho = maeEvaforKnotRemoval(keep_knots, keep_coeffs, pts, y_pos_flags, pct)
+        mae_iter_ortho = np.array(mae_iter_ortho)
+        remove_idx = mae_iter_ortho.argmin() + 1 # jump ahead since mae_iter_ortho only coresponding to inner knots 
+        keep_idx = list(range(0, len(keep_knots)))
+        removed = keep_idx.pop(remove_idx)
+    else:
+        keep_idx = [0, len(keep_knots)-1]
+
+    return keep_idx
+
+#%%
+def maeEvaforKnotRemoval(knot_set, coeff_set, pts, y_pos_flags, pct):
+    mae_iter_ortho = []
+    for i in range(1, len(knot_set) - 1): # evaluating only inner knots
+        remove_idx = i
+        new_knot_set = np.delete(knot_set, remove_idx)
+        new_coeff_set = np.delete(coeff_set, remove_idx)
+        
+        dist = calDistance(new_knot_set, new_coeff_set, pts)
+        
+        ortho_err = dist.min(axis=1)
+        ortho_err[y_pos_flags] = ortho_err[y_pos_flags] * pct
+        ortho_err[~y_pos_flags] = ortho_err[~y_pos_flags] * (100 - pct) 
+        mae_iter_ortho.append(ortho_err.mean())
+    
+    return mae_iter_ortho
+
+#%%
+def calBIC(ortho_err, knot_set, penalty):
+    # BIC acccumulation
+    positive_flags = ortho_err > 0 # in case a value is exactly 0
+    
+    
+    pars = stats.lognorm.fit(ortho_err[positive_flags])
+    loglik = -1 * stats.lognorm.nnlf(pars, ortho_err[positive_flags])
+
+    num_segs = len(knot_set)-1
+
+    bic_remove = -2 * loglik + penalty * num_segs * math.log(len(ortho_err))
+ 
+    return bic_remove
 
 #%%
 def nita_px(px, date_vec, doy_vec, 
@@ -41,10 +240,12 @@ def nita_px(px, date_vec, doy_vec,
         
     #---
     # 0.5 prepare x and y 
+        # make a copy of orignial data pairs 
+    
         x = date_vec 
         y = px 
     
-        x, y, doy_vec = uf.filterLimits(x, y, doy_vec, value_limits, date_limits, doy_limits)
+        x, y, doy_vec = filterLimits(x, y, doy_vec, value_limits, date_limits, doy_limits)
     
         noise = np.median(np.absolute(np.diff(y)))
     
@@ -67,8 +268,8 @@ def nita_px(px, date_vec, doy_vec,
     
         pts = np.column_stack((x, y))
     
-        dist_init = uf.calDistance(knot_set, coeff_set, pts)
-        mae_lin = uf.calMae(dist_init)
+        dist_init = calDistance(knot_set, coeff_set, pts)
+        mae_lin = calMae(dist_init)
 
     # ---
     # 2. NITA build
@@ -79,15 +280,15 @@ def nita_px(px, date_vec, doy_vec,
         
             for i in range(1, max_complex):
                 
-                dist = uf.calDistance(knot_set, coeff_set, pts)
-                cand_loc, coeff = uf.findCandidate(dist, filt_dist, pct, y, loc_set, filter_opt);
+                dist = calDistance(knot_set, coeff_set, pts)
+                cand_loc, coeff = findCandidate(dist, filt_dist, pct, y, loc_set, filter_opt);
             
                 if cand_loc == -999:
                     break
             
-                knot_set, coeff_set, loc_set = uf.updateknotcoeffSet(knot_set, coeff_set, loc_set, x, cand_loc, coeff)
-                dist_new = uf.calDistance(knot_set, coeff_set, pts)
-                mae_ortho.append(uf.calMae(dist_new))
+                knot_set, coeff_set, loc_set = updateknotcoeffSet(knot_set, coeff_set, loc_set, x, cand_loc, coeff)
+                dist_new = calDistance(knot_set, coeff_set, pts)
+                mae_ortho.append(calMae(dist_new))
                 del dist, dist_new, cand_loc, coeff 
             
             complexity_count = len(knot_set)-1
@@ -115,28 +316,28 @@ def nita_px(px, date_vec, doy_vec,
         
             knot_storage.append(knot_set)
             coeff_storage.append(coeff_set) 
-            dist_init = uf.calDistance(knot_set, coeff_set, pts)
-            mae_ortho_holder.append(uf.calMae(dist_init)) 
+            dist_init = calDistance(knot_set, coeff_set, pts)
+            mae_ortho_holder.append(calMae(dist_init)) 
             ortho_err = dist_init.min(axis=1)
             ortho_err[y_pos_flags] = ortho_err[y_pos_flags] * pct
             ortho_err[~y_pos_flags] = ortho_err[~y_pos_flags] * (100 - pct)
-            bic_remove.append(uf.calBIC(ortho_err, knot_set, penalty))
+            bic_remove.append(calBIC(ortho_err, knot_set, penalty))
         
             for i in range(1, end_count):
-                keep_loc = uf.genKeepIdx(knot_set, coeff_set, pts, pct, y_pos_flags)
+                keep_loc = genKeepIdx(knot_set, coeff_set, pts, pct, y_pos_flags)
             
                 knot_set = knot_set[keep_loc]
                 coeff_set = coeff_set[keep_loc]
                 knot_storage.append(knot_set)
                 coeff_storage.append(coeff_set)
             
-                dist = uf.calDistance(knot_set, coeff_set, pts)
-                mae_ortho_holder.append(uf.calMae(dist))
+                dist = calDistance(knot_set, coeff_set, pts)
+                mae_ortho_holder.append(calMae(dist))
                 ortho_err = dist.min(axis=1)
                 ortho_err[y_pos_flags] = ortho_err[y_pos_flags] * pct
                 ortho_err[~y_pos_flags] = ortho_err[~y_pos_flags] * (100 - pct)
             
-                bic_remove.append(uf.calBIC(ortho_err, knot_set, penalty))
+                bic_remove.append(calBIC(ortho_err, knot_set, penalty))
                 del dist, ortho_err
             
             bic_idx = int(np.where(bic_remove == min(bic_remove))[0])
@@ -179,4 +380,42 @@ def nita_px(px, date_vec, doy_vec,
                    'pts': pts}
 
     return results_dic    
+
+#%%
+def viewNITA(px, date_vec, doy_vec, results_dic, showdata='fit', colorbar='off'):
+    try:
+        knot_set = results_dic['final_knots']
+        coeff_set = results_dic['final_coeffs']
+        pts = results_dic['pts']
+        fit_x = pts[:,0]
+        fit_y = pts[:,1]
+        bail_cut = results_dic['mae_linear'] / results_dic['noise']
+        fit_count = len(fit_y)
+        
+        fig, ax = plt.subplots()
+        if showdata == 'all':
+            i = ax.scatter(date_vec, px, c=doy_vec)
+            ax.plot(knot_set, coeff_set, 'ro', mfc='none')
+            ax.plot(knot_set, coeff_set, 'r-')
+            ax.set_xlim([date_vec.min(), date_vec.max()])
+            ax.set_ylim([px.min(), px.max()])
+            if colorbar == 'on':
+                fig.colorbar(i)
+        elif showdata == 'fit':
+            doy_fit = np.round(((fit_x/1000 - np.floor_divide(fit_x, 1000)) * 365))
+            i = ax.scatter(fit_x, fit_y, c=doy_fit)
+            ax.plot(knot_set, coeff_set, 'ro', mfc='none')
+            ax.plot(knot_set, coeff_set, 'r-')
+            ax.set_xlim([fit_x.min(), fit_x.max()])
+            ax.set_ylim([fit_y.min(), fit_y.max()])
+            if colorbar == 'on':
+                fig.colorbar(i)
+        else:
+            raise ValueError('in-valid showdata option!')
+        print('bail_cut = %s \nfit_count = %s' % (bail_cut, fit_count))
+        
+    except: 
+        plt.plot()
+        plt.text(0, 0, 'something\'s wrong')
+            
             
---
+++ pynita/data_reader/ini_reader.py
@@ -0,0 +1,312 @@
+"""
+Read in settings from configuration file *.ini
+Created on Jun 4, 2018
+@author: Leyang Feng
+@email: feng@american.edu
+@Project: pynita
+License:  
+Copyright (c) 
+"""
+
+import os
+from configobj import ConfigObj
+
+class ConfigReader:
+
+    def __init__(self, ini):
+
+        c = ConfigObj(ini)
+
+        p = c['Project']
+
+        # project dirs
+        self.root = p['RootDir']
+        self.ProjectName = p['ProjectName']
+        self.InputFolder = os.path.join(self.root, p['InputFolder'])
+        self.OutDir = self.create_dir(os.path.join(self.root, p['OutputFolder']))
+        self.OutputFolder = self.create_dir(os.path.join(self.OutDir, self.ProjectName))
+
+        # see if modules are in config file
+        try:
+            v = c['VI']
+        except KeyError:
+            v = False
+
+
+        try:
+            np = c['NITAParameters']
+        except KeyError:
+            np = False
+
+        # project level settings
+        self.ncell = 67420
+        self.ngridrow = 360
+        self.ngridcol = 720
+        self.OutputUnitStr = None
+        self.HistFlag = p['HistFlag']
+        self.StartYear = int(p['StartYear'])
+        self.EndYear = int(p['EndYear'])
+        self.nmonths = (self.EndYear - self.StartYear + 1) * 12
+        self.OutputFormat = int(p['OutputFormat'])
+        self.OutputUnit = int(p['OutputUnit'])
+        self.OutputInYear = int(p['OutputInYear'])
+        self.AggregateRunoffBasin = int(p['AggregateRunoffBasin'])
+        self.AggregateRunoffCountry = int(p['AggregateRunoffCountry'])
+        self.AggregateRunoffGCAMRegion = int(p['AggregateRunoffGCAMRegion'])
+        self.PerformDiagnostics = int(p['PerformDiagnostics'])
+        self.CreateTimeSeriesPlot = int(p['CreateTimeSeriesPlot'])
+        self.CalculateAccessibleWater = int(p['CalculateAccessibleWater'])
+        self.CalculateHydropowerPotential = int(p['CalculateHydropowerPotential'])
+        self.CalculateHydropowerActual = int(p['CalculateHydropowerActual'])
+
+        # pet
+        if pt is not False:
+            self.pet_module = pt['pet_module'].lower()
+
+            if self.pet_module == 'hargreaves':
+                pass
+
+            elif self.pet_module == 'penman-monteith':
+                pass
+
+            elif self.pet_module == 'none':
+                pass
+
+            else:
+                msg = "ERROR: PET module '{0}' not found. Please check spelling and try again.".format(self.pet_module)
+                raise RuntimeError(msg)
+        else:
+            self.pet_module = 'none'
+
+        print 'MODULE!!!!: ', self.pet_module
+
+        # runoff
+        if ro is not False:
+            self.runoff_module = ro['runoff_module'].lower()
+
+            if self.runoff_module == 'gwam':
+
+                ro_mod = ro['GWAM']
+                self.ro_model_dir = os.path.join(self.RunoffDir, ro_mod['model_dir'])
+                self.SpinUp = int(ro_mod['SpinUp']) * 12
+                self.MaxSoilMois = os.path.join(self.ro_model_dir, ro_mod['MaxSoilMois'])
+                self.LakesMSM = os.path.join(self.ro_model_dir, ro_mod['LakesMSM'])
+                self.AdditWaterMSM = os.path.join(self.ro_model_dir, ro_mod['AdditWaterMSM'])
+
+                # channel storage file full path name with extension if running future
+                self.ChStorageFile = None
+                self.ChStorageVarName = None
+
+                # soil moisture file full path name with extension if running future
+                self.SavFile = None
+                self.SavVarName = None
+
+                if self.HistFlag == 'False':
+                    try:
+                        self.ChStorageFile = ro_mod['ChStorageFile']
+                        self.ChStorageVarName = ro_mod['ChStorageVarName']
+                        self.SavFile = ro_mod['SavFile']
+                        self.SavVarName = ro_mod['SavVarName']
+
+                    except KeyError:
+                        raise RuntimeError("Error: ChStorageFile and ChStorageVarName are not defined for Future Mode.")
+
+            elif self.runoff_module == 'abcd':
+
+                ro_mod = ro['abcd']
+                self.ro_model_dir = os.path.join(self.RunoffDir, ro_mod['model_dir'])
+                self.calib_file = os.path.join(self.ro_model_dir, ro_mod['calib_file'])
+                self.SpinUp = int(ro_mod['SpinUp'])
+                self.ro_jobs = int(ro_mod['jobs'])
+
+            elif self.runoff_module == 'none':
+                pass
+
+            else:
+                raise RuntimeError("ERROR: Runoff module '{0}' not found. Please check spelling and try again.".format(self.runoff_module))
+        else:
+            self.runoff_module = 'none'
+
+        # routing
+        if rt is not False:
+            self.routing_module = rt['routing_module'].lower()
+
+            if self.routing_module == 'mrtm':
+                rt_mod = rt['MRTM']
+                self.rt_model_dir = os.path.join(self.RoutingDir, rt_mod['model_dir'])
+                self.strm_veloc = os.path.join(self.rt_model_dir, rt_mod['ChVeloc'])
+                self.FlowDis = os.path.join(self.rt_model_dir, rt_mod['FlowDis'])
+                self.FlowDir = os.path.join(self.rt_model_dir, rt_mod['FlowDir'])
+
+                try:
+                    self.routing_spinup = int(rt_mod['routing_spinup'])
+                except KeyError:
+                    self.routing_spinup = self.nmonths
+
+                try:
+                    self.alt_runoff = self.custom_runoff(rt_mod['alt_runoff'])
+                except KeyError:
+                    self.alt_runoff = None
+
+            elif self.routing_module == 'none':
+                pass
+
+            else:
+                raise RuntimeError("ERROR: Routing module '{0}' not found. Please check spelling and try again.".format(self.routing_module))
+
+        else:
+            self.routing_module = 'none'
+
+        # create model configuration string (pet_runoff_routing)
+        self.mod_cfg = '{0}_{1}_{2}'.format(self.pet_module, self.runoff_module, self.routing_module)
+
+        if self.mod_cfg == 'none_none_none':
+            raise RuntimeError('No PFT, Runoff, or Routing model selected.')
+
+        # climate
+        if clm is not False:
+            self.climate = True
+            self.OutputNameStr = clm['climate_scenario']
+            self.PrecipitationFile = os.path.join(self.ClimateFolder, clm['PrecipitationFile'])
+            self.PrecipVarName = clm['PrecipVarName']
+            self.TemperatureFile = os.path.join(self.ClimateFolder, clm['TemperatureFile'])
+            self.TempVarName = clm['TempVarName']
+            self.DailyTemperatureRangeFile = os.path.join(self.ClimateFolder, clm['DailyTemperatureRangeFile'])
+            self.DTRVarName = clm['DTRVarName']
+        else:
+            self.climate = False
+            self.OutputNameStr = self.ProjectName.replace(' ', '_').replace(',', '_').replace(';', '_')
+            print('WARNING:  No climate data selected for use.')
+
+        # reference
+        if r is not False:
+            self.Area = os.path.join(self.Reference, r['Area'])
+            self.Coord = os.path.join(self.Reference, r['Coord'])
+            self.BasinIDs = os.path.join(self.Reference, r['BasinIDs'])
+            self.BasinNames = os.path.join(self.Reference, r['BasinNames'])
+            self.GCAMRegionIDs = os.path.join(self.Reference, r['GCAMRegionIDs'])
+            self.GCAMRegionNames = os.path.join(self.Reference, r['GCAMRegionNames'])
+            self.CountryIDs = os.path.join(self.Reference, r['CountryIDs'])
+            self.CountryNames = os.path.join(self.Reference, r['CountryNames'])
+        else:
+            print('WARNING:  No reference data selected for use.')
+
+        self.CellArea = None
+        self.ChSlope = None
+        self.DrainArea = None
+        self.RiversMSM = None
+
+        # diagnostics
+        if d is not False:
+            if self.PerformDiagnostics:
+                self.VICDataFile = os.path.join(self.DiagDir, d['VICDataFile'])
+                self.UNHDataFile = os.path.join(self.DiagDir, d['UNHDataFile'])
+                self.WBMDataFile = os.path.join(self.DiagDir, d['WBMDataFile'])
+                self.WBMCDataFile = os.path.join(self.DiagDir, d['WBMCDataFile'])
+                self.DiagnosticScale = int(d['Scale'])
+
+        # plots
+        if t is not False:
+            if self.CreateTimeSeriesPlot:
+                self.TimeSeriesScale = int(t['Scale'])
+                self.TimeSeriesMapID = 999
+
+                try:
+                    l = int(t['MapID'])
+                    self.TimeSeriesMapID = l
+
+                except:
+                    # as list
+                    l = map(int, t['MapID'])
+                    self.TimeSeriesMapID = l
+
+        # accessible water
+        if a is not False:
+            if self.CalculateAccessibleWater:
+                self.ResCapacityFile = os.path.join(self.AccWatDir, a['ResCapacityFile'])
+                self.BfiFile = os.path.join(self.AccWatDir, a['BfiFile'])
+                self.HistEndYear = int(a['HistEndYear'])
+                self.GCAM_StartYear = self.ck_year(int(a['GCAM_StartYear']))
+                self.GCAM_EndYear = int(a['GCAM_EndYear'])
+                self.GCAM_YearStep = int(a['GCAM_YearStep'])
+                self.MovingMeanWindow = int(a['MovingMeanWindow'])
+                self.Env_FlowPercent = float(a['Env_FlowPercent'])
+
+                if (self.StartYear > self.GCAM_StartYear) or (self.EndYear < self.GCAM_EndYear):
+                    raise RuntimeError("Accessible water range of GCAM years are outside the range of years in climate data.")
+
+        # hydropower potential
+        if hp is not False:
+            if self.CalculateHydropowerPotential:
+                self.hpot_start_date = hp['hpot_start_date']
+                self.q_ex = float(hp['q_ex'])
+                self.ef = float(hp['ef'])
+                self.GridData = os.path.join(self.HydActDir, ha['GridData'])
+
+        # hydropower actual
+        if ha is not False:
+            if self.CalculateHydropowerActual:
+                self.hact_start_date = ha['hact_start_date']
+                self.HydroDamData = os.path.join(self.HydActDir, ha['HydroDamData'])
+                self.MissingCap = os.path.join(self.HydActDir, ha['MissingCap'])
+                self.rule_curves = os.path.join(self.HydActDir, ha['rule_curves'])
+                self.GridData = os.path.join(self.HydActDir, ha['GridData'])
+                self.DrainArea = os.path.join(self.HydActDir, ha['DrainArea'])
+
+    def ck_year(self, yr):
+        """
+        Check to see if the target year is within the bounds of the data.
+        """
+        if (yr < self.StartYear) or (yr > self.EndYear):
+            raise RuntimeError("Accessible water year {0} is outside the range of years in the climate data.".format(yr))
+        else:
+            return yr
+
+    def custom_runoff(self, f):
+        """
+        Check for custom runoff file name.  If 'none', return None; else
+        return full path to file.
+        :param f:
+        :return:
+        """
+        if f == 'none':
+            return None
+        else:
+            return os.path.join(self.rt_model_dir, f)
+
+    def create_dir(self, pth):
+        """
+        Check to see if the target path is exists.
+        """
+        if os.path.isdir(pth) is False:
+            os.mkdir(pth)
+        return pth
+
+    def log_info(self):
+
+        print('ProjectName : {}'.format(self.ProjectName))
+        print('InputFolder : {}'.format(self.InputFolder))
+        print('OutputFolder: {}'.format(self.OutputFolder))
+        print('StartYear - End Year        : {0}-{1}'.format(self.StartYear, self.EndYear))
+        print('Number of Months            : {}'.format(self.nmonths))
+
+        if self.climate:
+            print('Precipitation File          : {}'.format(self.PrecipitationFile))
+            print('Temperature File            : {}'.format(self.TemperatureFile))
+            print('Daily Temperature Range File: {}'.format(self.DailyTemperatureRangeFile))
+
+        if self.HistFlag.lower() in ['true', 't', 'yes', 'y', '1']:
+            print('Running: Historic Mode')
+
+        else:
+            print('Running:  Future Mode')
+            try:
+                print('Historic Soil Moisture File: {}'.format(self.SavFile))
+                print('Historic Channel Storage File: {}'.format(self.ChStorageFile))
+            except AttributeError:
+                pass
+
+        try:
+            print('Diagnostics will be performed using the data file: {}'.format(self.VICDataFile))
+        except AttributeError:
+            pass
