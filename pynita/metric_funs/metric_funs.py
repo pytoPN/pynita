2acdc86e57d7d003cd3066765ac68b89741d34e3
Leyang Feng <feng@american.edu>
2018-07-09 15:36:54 -0400

    Added some metrics methods

--- pynita/metric_funs/metric_funs.py
+++ pynita/metric_funs/metric_funs.py
@@ -9,6 +9,8 @@ Copyright (c)
 """
 
 import numpy as np 
+from scipy import ndimage
+import matplotlib.pyplot as plt
 
 def computeMetrics(results_dic, vi_change_thresh, run_thresh, time_step):
     
@@ -17,107 +19,113 @@ def computeMetrics(results_dic, vi_change_thresh, run_thresh, time_step):
     knots = results_dic['final_knots'];
     coeffs = results_dic['final_coeffs'];
     rises = results_dic['rises'];
-    runs = results_dic['runs'];
+    #runs = results_dic['runs'];
     runs_in_days = results_dic['runs_days'];
-
-    try:
-        # ---
-        # 2. interpolation 
-        knot_first = np.floor(knots[0]/1000)
-        knot_last = np.floor(knots[-1]/1000)+1
-        all_knots_dis = np.sort(np.unique(np.concatenate((np.arange(knot_first, knot_last+1, time_step) * 1000, knots))))
-        all_knots_dis = all_knots_dis[(all_knots_dis >= knots[0]) & (all_knots_dis <= knots[-1])]
-        all_coeffs_interp = np.interp(all_knots_dis, knots, coeffs) 
-        interp_pts = np.column_stack((all_knots_dis, all_coeffs_interp)) # output 
-        
-        # ---
-        # 3. change percent and slope calculation 
-        change_percent = rises/abs(coeffs[0:-1])
-        dist_flags = (change_percent<vi_change_thresh) & (runs_in_days<=run_thres)
-        
-        # 4. disturbance detection 
-        start_ls = []
-        end_ls = [] 
-        n = 0
-        m = 0
-        for i in range(len(dist_flags)):
-            if dist_flags[i]:
-                n = n + 1
-                if m == 1:
-                    start_ls.append(i)
-                m = 0
-            else:
-                if n != 0:
-                    end_ls.append(i-1)  
-                n = 0
-                m = 1
-                
-        dist_locs = [(start_ls[i], end_ls[i]+1) for i in range(len(start_ls))] 
-        
-        # ---
-        # 5. disturbance metric calculation 
-        # 5.a no disturbance 
-        if len(dist_locs) == 0: 
-            num_dist = 0
-            cum_mag_dist = -999
-            dist_date_before = -999
-            dist_date_nadir = -999
-            dist_duration = -999
-            dist_slope = -999
-            dist_coeff_nadir = -999
-            post_dist_slope = -999
-            post_dist_mag = -999
-            dist_mag = -999
-            dist_coeff_before = -999
-        # 5.b get metrics for largest disturbance as default 
-        else: 
-            num_dist = len(dist_locs) # output 
-            
-            dist_mags = []
-            for dist_loc in dist_locs:
-                coeff_st = coeffs[dist_loc[0]]
-                coeff_ed = coeffs[dist_loc[1]]
-                mag = coeff_st-coeff_ed
-                dist_mags.append(mag)
-            cum_mag_dist = sum(dist_mags) # output 
-            
-            dist_mags = np.array(dist_mags)
-            dist_idx = int(np.where(dist_mags==dist_mags.min())[0])            
-            dist_loc = dist_locs[dist_idx]
-            
-            dist_date_before = knots[dist_loc[0]] # output 
-            dist_date_nadir = knots[dist_loc[1]] # output 
-            dist_duration = dist_date_nadir - dist_date_before # output
-            
-            dist_coeff_before = coeffs[dist_loc[0]] # output 
-            dist_coeff_nadir = coeffs[dist_loc[1]] # output 
-            dist_mag = dist_coeff_before - dist_coeff_nadir # output 
-            
-            dist_slope = -dist_mag/dist_duration # output 
-            
-            # first 'recovery' or just first segment after default disturbance 
-            if dist_loc[1] == (len(knots)-1): # the case that the disturbance is the last sagment 
-                post_dist_slope = -999 # output 
-                post_dist_mag = -999 
-            else:
-                next_loc = [dist_loc[1], dist_loc[1]+1]
-                post_dist_mag = coeffs[next_loc[1]] - coeffs[next_loc[0]] # output 
-                post_dist_slope = post_dist_mag / (knots[next_loc[1]] - knots[next_loc[0]]) # output 
-                  
-    except:
-        num_dist = -999
-        cum_mag_dist = -999
-        dist_date_before = -999
-        dist_date_nadir = -999
-        dist_duration = -999
-        dist_slope = -999
-        dist_coeff_nadir = -999
-        post_dist_slope = -999
-        post_dist_mag = -999
-        dist_mag = -999
-        dist_coeff_before = -999
-        interp_pts = -999 
     
+    if knots[0] != -999:
+    
+        try:
+            # ---
+            # 2. interpolation 
+            knot_first = np.floor(knots[0]/1000)
+            knot_last = np.floor(knots[-1]/1000)+1
+            all_knots_dis = np.sort(np.unique(np.concatenate((np.arange(knot_first, knot_last+1, time_step) * 1000, knots))))
+            all_knots_dis = all_knots_dis[(all_knots_dis >= knots[0]) & (all_knots_dis <= knots[-1])]
+            all_coeffs_interp = np.interp(all_knots_dis, knots, coeffs) 
+            interp_pts = np.column_stack((all_knots_dis, all_coeffs_interp)) # output 
+            
+            # ---
+            # 3. change percent and slope calculation 
+            change_percent = rises/abs(coeffs[0:-1])
+            dist_flags = (change_percent<vi_change_thresh) & (runs_in_days<=run_thresh)
+            dist_bin = [1 if flag else 0 for flag in dist_flags]
+            label, num_features = ndimage.label(dist_bin)
+            
+            # 4. disturbance detection
+            dist_locs = []
+            for i in range(num_features):
+                label_val = i + 1 
+                locs = np.where(label == label_val)[0]
+                dist_locs.append((locs[0], locs[-1]+1))
+            
+            # ---
+            # 5. disturbance metric calculation 
+            # 5.a no disturbance 
+            if len(dist_locs) == 0: 
+                num_dist = 0
+                cum_mag_dist = np.nan
+                dist_date_before = np.nan
+                dist_date_nadir = np.nan
+                dist_duration = np.nan
+                dist_slope = np.nan
+                dist_coeff_nadir = np.nan
+                post_dist_slope = np.nan
+                post_dist_mag = np.nan
+                dist_mag = np.nan
+                dist_coeff_before = np.nan
+            # 5.b get metrics for largest disturbance as default 
+            else: 
+                num_dist = len(dist_locs) # output 
+                
+                dist_mags = []
+                for dist_loc in dist_locs:
+                    coeff_st = coeffs[dist_loc[0]]
+                    coeff_ed = coeffs[dist_loc[1]]
+                    mag = coeff_st-coeff_ed
+                    dist_mags.append(mag)
+                cum_mag_dist = sum(dist_mags) # output 
+                
+                dist_mags = np.array(dist_mags)
+                dist_idx = int(np.where(dist_mags==dist_mags.min())[0])            
+                dist_loc = dist_locs[dist_idx]
+                
+                dist_date_before = knots[dist_loc[0]] # output 
+                dist_date_nadir = knots[dist_loc[1]] # output 
+                dist_duration = dist_date_nadir - dist_date_before # output
+                
+                dist_coeff_before = coeffs[dist_loc[0]] # output 
+                dist_coeff_nadir = coeffs[dist_loc[1]] # output 
+                dist_mag = dist_coeff_before - dist_coeff_nadir # output 
+                
+                dist_slope = -dist_mag/dist_duration # output 
+                
+                # first 'recovery' or just first segment after default disturbance 
+                if dist_loc[1] == (len(knots)-1): # the case that the disturbance is the last sagment 
+                    post_dist_slope = np.nan # output 
+                    post_dist_mag = np.nan
+                else:
+                    next_loc = [dist_loc[1], dist_loc[1]+1]
+                    post_dist_mag = coeffs[next_loc[1]] - coeffs[next_loc[0]] # output 
+                    post_dist_slope = post_dist_mag / (knots[next_loc[1]] - knots[next_loc[0]]) # output 
+                      
+        except:
+            num_dist = np.nan
+            cum_mag_dist = np.nan
+            dist_date_before = np.nan
+            dist_date_nadir = np.nan
+            dist_duration = np.nan
+            dist_slope = np.nan
+            dist_coeff_nadir = np.nan
+            post_dist_slope = np.nan
+            post_dist_mag =np.nan
+            dist_mag = np.nan
+            dist_coeff_before = np.nan
+            interp_pts = np.nan
+    
+    else: 
+        num_dist = np.nan
+        cum_mag_dist = np.nan
+        dist_date_before = np.nan
+        dist_date_nadir = np.nan
+        dist_duration = np.nan
+        dist_slope = np.nan
+        dist_coeff_nadir = np.nan
+        post_dist_slope = np.nan
+        post_dist_mag =np.nan
+        dist_mag = np.nan
+        dist_coeff_before = np.nan
+        interp_pts = np.nan
+        
     metrics_dic = {'num_dist': num_dist,
                    'cum_mag_dist': cum_mag_dist,
                    'dist_date_before': dist_date_before, 
@@ -133,7 +141,48 @@ def computeMetrics(results_dic, vi_change_thresh, run_thresh, time_step):
         
     return metrics_dic
 
+#%%
+#def stretchMI(MI_1d):
 
-        
-                
-                
+#%%
+def plotMI(MI_2d): 
+    fig, ax = plt.subplots()  
+    mappable = ax.matshow(MI_2d)
+    fig.colorbar(mappable)
+
+
+#%%          
+def MI_complexity(results_dics):
+    values_1d = np.array([dic['complexity'] for dic in results_dics])
+    
+    return(values_1d)
+
+#%%
+def calDistDate(metrics_dic, option='middle'):
+    dist_date_before = metrics_dic['dist_date_before']
+    dist_date_nadir = metrics_dic['dist_date_nadir']
+
+    if option == 'beginning':
+        dist_date = dist_date_before
+    elif option == 'middle':
+        dist_date = (dist_date_before + dist_date_nadir) / 2
+    elif option == 'end':
+        dist_date = dist_date_nadir
+    else: 
+        raise RuntimeError('ERROR: invalid option!')
+    
+    return dist_date
+    
+#%%
+def MI_distDate(metrics_dics, option='middle'):
+    vals_1d = np.array([calDistDate(metrics_dic, option=option) for metrics_dic in metrics_dics])
+    
+    return(vals_1d)
+
+#%%
+def MI_distDuration(metrics_dics): # in days
+    dist_durations = [metrics_dic['dist_duration'] for metrics_dic in metrics_dics] 
+    vals_1d = np.array([np.floor(duration/1000)*365+np.mod(duration, 1000)/1000*365 for duration in dist_durations])
+    
+    return vals_1d
+    
--- pynita/nita.py
+++ pynita/nita.py
@@ -13,8 +13,9 @@ from multiprocessing import Pool
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
-from data_reader.data_loader import DataLoader
+from data_reader.data_loader import dataLoader
 from data_reader.ini_reader import ConfigReader
+from data_writer import data_writer_funs as dw
 from nita_funs import nita_funs as nf
 from metric_funs import metric_funs as mf
 import utils.logging as lg
@@ -45,7 +46,7 @@ class nitaObj:
             lg.closeLogger(self.logger)
             
     def loadPts(self, info_column='none', full_table=False):
-        dl = DataLoader(self.cfg)
+        dl = dataLoader(self.cfg)
         self.pts = dl.load_pts(info_column, full_table)
         
         # properties
@@ -54,8 +55,8 @@ class nitaObj:
         self.ref_pts = dl.ref_tb        
         
     def loadStack(self):
-        dl = DataLoader(self.cfg)
-        self.stack, self.stack_dates, self.stack_doy, self.stack_prj = dl.load_stack()
+        dl = dataLoader(self.cfg)
+        self.stack, self.stack_dates, self.stack_doy, self.stack_prj, self.stack_geotransform = dl.load_stack()
         
         # properties
         self.stack_shape = '{0} rows {1} columns {2} layers'.format(self.stack.shape[1], self.stack.shape[2], self.stack.shape[0])
@@ -145,6 +146,13 @@ class nitaObj:
 
     def runStack(self, parallel=True, workers=2):
  
+        # reload in case anything changed in the ini file 
+        # TODO: got to be a better way to arrange this -- 
+        # ini is needed in the __init__ for path and etc. but 
+        # can change before running points  
+        
+        self.cfg = ConfigReader(ini) 
+        
         # ---
         # 1.
         # initialize log if wanted
@@ -171,7 +179,7 @@ class nitaObj:
             compute_mask = np.ones(self.stack.shape[1:3])
         else:
             compute_mask = self.compute_mask
-            if self.compute_mask.shape != self.stack.shape[1:3]:
+            if compute_mask.shape != self.stack.shape[1:3]:
                 raise RuntimeError('ERROR: user_mask dimensions does not match stack dimensions! \nUse setMask() to reset')
         
         # ---
@@ -289,6 +297,84 @@ class nitaObj:
 
         return results_dic 
     
+    def runPixel(self, xy_pair, 
+                 use_compute_mask=False, 
+                 plot=True, showdata='fit', colorbar=True, 
+                 **nita_parameters):
+        
+        # get n and m 
+        n = xy_pair[1]
+        m = xy_pair[0]
+        
+        # get the compute mask value 
+        if use_compute_mask:
+            if type(self.compute_mask).__name__ != 'ndarray':
+                compute_mask = True
+            else:
+                compute_mask_mat = self.compute_mask
+                if compute_mask_mat.shape != self.stack.shape[1:3]:
+                    raise RuntimeError('ERROR: user_mask dimensions does not match stack dimensions! \nUse setMask() to reset')
+                else:
+                    compute_mask = compute_mask_mat[n, m]
+        else:
+            compute_mask = True         
+
+        # get the nita parameters
+        param_dic = {}
+        param_dic['date_vec'] = self.stack_dates
+        param_dic['doy_vec'] = self.stack_doy
+        param_dic['value_limits'] = self.cfg.value_limits
+        param_dic['doy_limits'] = self.cfg.doy_limits
+        param_dic['date_limits'] = self.cfg.date_limits
+        param_dic['bail_thresh'] = self.cfg.bail_thresh
+        param_dic['noise_thresh'] = self.cfg.noise_thresh       
+        param_dic['penalty'] = self.cfg.penalty    
+        param_dic['filt_dist'] = self.cfg.filt_dist
+        param_dic['pct'] = self.cfg.pct      
+        param_dic['max_complex'] = self.cfg.max_complex
+        param_dic['min_complex'] = self.cfg.min_complex
+        param_dic['filter_opt'] = self.cfg.filter_opt       
+        
+        if nita_parameters is not None:       
+            keys = nita_parameters.keys()
+            wrong_names = [key for key in keys if not key in param_dic.keys()]
+            
+            if len(wrong_names) == 0:
+                for key, value in nita_parameters.items():
+                    param_dic[key] = value
+            else:
+                raise RuntimeError('ERROR: Wrong parameter name!')
+                
+        date_vec     = param_dic['date_vec']     
+        doy_vec      = param_dic['doy_vec']      
+        value_limits = param_dic['value_limits'] 
+        doy_limits   = param_dic['doy_limits']   
+        date_limits  = param_dic['date_limits']  
+        bail_thresh  = param_dic['bail_thresh']  
+        noise_thresh = param_dic['noise_thresh']   
+        penalty      = param_dic['penalty']      
+        filt_dist    = param_dic['filt_dist']    
+        pct          = param_dic['pct']          
+        max_complex  = param_dic['max_complex']  
+        min_complex  = param_dic['min_complex']  
+        filter_opt   = param_dic['filter_opt']        
+                   
+        # get data and run 
+        date_vec = self.stack_dates   
+        doy_vec = self.stack_doy
+        px = self.stack[:, n, m]
+        
+        results_dic = nf.nita_px(px, date_vec, doy_vec, 
+                                 value_limits, doy_limits, date_limits,
+                                 bail_thresh, noise_thresh,
+                                 penalty, filt_dist, pct, max_complex, min_complex,
+                                 compute_mask, filter_opt)
+        
+        if plot:
+            nf.viewNITA(px, date_vec, doy_vec, results_dic, showdata=showdata, colorbar=colorbar)
+        
+        return results_dic    
+    
     def computeStackMetrics(self, parallel=True, workers=2):
         
         # check if self.stack_results exists 
@@ -302,7 +388,13 @@ class nitaObj:
         time_step = self.cfg.time_step
         
         if parallel:
-            pass
+            
+            iterable = [(results_dic, vi_change_thresh, run_thresh, time_step) for results_dic in self.stack_results]
+        
+            pool = Pool(workers)
+            metrics_dics_1d = pool.starmap(mf.computeMetrics, iterable)
+            pool.close()
+            pool.join()
         
         if not parallel: 
             metrics_dics_1d = []
@@ -312,28 +404,130 @@ class nitaObj:
         
         self.stack_metrics = metrics_dics_1d
         
- #   def getPixelMetrics(self, xy_pair):
-  #      pass
+    def getPixelMetrics(self, xy_pair):
+        
+        try:
+            type(self.stack_metrics)
+        except AttributeError:
+            raise RuntimeError('stack metrics not calculated yet!')        
+
+        n = xy_pair[1]
+        m = xy_pair[0]
+        
+        stack_shape = self.stack.shape # (t, n, m)
+        nm_vec = np.arange(0,  stack_shape[1]*stack_shape[2])
+        n_vec = np.floor_divide(nm_vec, stack_shape[2])
+        m_vec = np.mod(nm_vec, stack_shape[2])
+        
+        pixel_idx = int(nm_vec[(n_vec == n) & (m_vec == m)])
+        
+        metrics_dic = self.stack_metrics[pixel_idx]
+
+        return metrics_dic 
     
-   # def computeMetrics(self, results_dic):
-    #    pass
+    def computeMetrics(self, results_dic, **metric_parameters):
+        
+        param_dic = {}
+        param_dic['vi_change_thresh'] = self.cfg.vi_change_thresh
+        param_dic['run_thresh'] = self.cfg.run_thresh
+        param_dic['time_step'] = self.cfg.time_step
+        
+        if metric_parameters is not None:       
+            keys = metric_parameters.keys()
+            wrong_names = [key for key in keys if not key in param_dic.keys()]
+            
+            if len(wrong_names) == 0:
+                for key, value in metric_parameters.items():
+                    param_dic[key] = value
+            else:
+                raise RuntimeError('ERROR: Wrong parameter name!')
+        
+        vi_change_thresh = param_dic['vi_change_thresh']
+        run_thresh = param_dic['run_thresh']
+        time_step = param_dic['time_step']
+
+        metrics_dic = mf.computeMetrics(results_dic, vi_change_thresh, run_thresh, time_step)
+     
+        return metrics_dic
     
-           
+    def MI_complexity(self, plot=True, save=True, fn='complexity.tiff'):
+        
+        # check if self.stack_results exists 
+        try:
+            type(self.stack_results)
+        except AttributeError:
+            raise RuntimeError('stack results not calculated yet!')
+        
+        vals_1d = mf.MI_complexity(self.stack_results)
+        stack_shape = self.stack.shape
+        vals_2d = vals_1d.reshape(stack_shape[1:3])
+        
+        if plot: 
+            mf.plotMI(vals_2d)
+        
+        if save:
+            dw.SaveIM(vals_2d, self.stack_prj, self.stack_geotransform,
+                      self.cfg.OutputFolder, fn)
+    
+    def MI_distDate(self, option='middle', plot=True, save=True, fn='distdate.tiff'):
+        
+        # check if self.stack_metrics exists 
+        try:
+            type(self.stack_metrics)
+        except AttributeError:
+            raise RuntimeError('stack metrics not calculated yet!')
+            
+        vals_1d = mf.MI_distDate(self.stack_metrics, option=option)
+        stack_shape = self.stack.shape
+        vals_2d = vals_1d.reshape(stack_shape[1:3])
+        
+        if plot: 
+            mf.plotMI(vals_2d)
+        
+        if save:
+            dw.SaveIM(vals_2d, self.stack_prj, self.stack_geotransform,
+                      self.cfg.OutputFolder, fn)               
+     
+    def MI_distDuration(self, plot=True, save=True, fn='distduration.tiff'):
+
+        # check if self.stack_metrics exists 
+        try:
+            type(self.stack_metrics)
+        except AttributeError:
+            raise RuntimeError('stack metrics not calculated yet!')
+            
+        vals_1d = mf.MI_distDuration(self.stack_metrics)
+        stack_shape = self.stack.shape
+        vals_2d = vals_1d.reshape(stack_shape[1:3])
+        
+        if plot: 
+            mf.plotMI(vals_2d)
+        
+        if save:
+            dw.SaveIM(vals_2d, self.stack_prj, self.stack_geotransform,
+                      self.cfg.OutputFolder, fn)           
+        
 if __name__ == '__main__':
     nita = nitaObj(ini)
     
     nita.startLog()
     
     # tests with points 
-    nita.loadPts(info_column='Name')
+    #nita.loadPts(info_column='Name')
     #nita.runPts([9999], compute_mask=True, plot=True, showdata='fit', colorbar=False, plot_title=True)
-    results_dic = nita.runPts([1], compute_mask=True, plot=True, showdata='fit', colorbar=True, plot_title=True)    
+    #results_dic = nita.runPts([1], compute_mask=True, plot=True, showdata='fit', colorbar=True, plot_title=True)    
     
     # tests with stack 
     nita.loadStack()
 
     #nita.runStack(parallel=True, workers=2)
     nita.runStack(parallel=False)
+    nita.computeStackMetrics(parallel=False)
     
+    results_dic = nita.runPixel([8, 5], use_compute_mask=False, **{'value_limits': [-0.5, 1], 'min_complex': 2})
+    metrics_dic = nita.computeMetrics(results_dic)
     
+    nita.MI_complexity(plot=True, save=True, fn='complexity.tiff')
+    nita.MI_distDate(option='middle', plot=True, save=True, fn='distdate.tiff')
+    nita.MI_distDuration(plot=True, save=True, fn='distduration.tiff')
     #nita.stopLog()
