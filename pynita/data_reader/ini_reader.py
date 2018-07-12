5bac9bb83a9dab44225c009e0353fff7ed54da68
Leyang Feng <feng@american.edu>
2018-07-12 17:29:19 -0400

    Moved the parameter from cfg attributes to a attribute dic.

--- pynita/data_reader/ini_reader.py
+++ pynita/data_reader/ini_reader.py
@@ -60,27 +60,28 @@ class ConfigReader:
         
         # module NITAParameters
         if np is not False: 
-            self.value_limits = [int(item) for item in np['value_limits']]
-            self.doy_limits = [int(item) for item in np['doy_limits']]
-            self.doy_limits = [self.doy_limits[i:i+2] for i in range(0, len(self.doy_limits), 2)]
-            self.date_limits = [int(item) for item in np['date_limits']]
-            self.bail_thresh = float(np['bail_thresh'])
-            self.noise_thresh = float(np['noise_thresh'])
-            self.penalty = float(np['penalty'])
-            self.filt_dist = int(np['filt_dist'])
-            self.pct = float(np['pct'])
-            self.max_complex = int(np['max_complex'])
-            self.min_complex = int(np['min_complex'])
-            self.filter_opt = np['filter_opt']
+            doy_limits = [int(item) for item in np['doy_limits']]
+            doy_limits = [doy_limits[i:i+2] for i in range(0, len(doy_limits), 2)]
+            self.param_nita = {'value_limits': [int(item) for item in np['value_limits']],
+                                'doy_limits': doy_limits,
+                                'date_limits': [int(item) for item in np['date_limits']], 
+                                'bail_thresh': float(np['bail_thresh']), 
+                                'noise_thresh': float(np['noise_thresh']),
+                                'penalty': float(np['penalty']),
+                                'filt_dist': int(np['filt_dist']),
+                                'pct': float(np['pct']),
+                                'max_complex': int(np['max_complex']),
+                                'min_complex': int(np['min_complex']),
+                                'filter_opt': np['filter_opt']}
             # TODO: add value check in here
         else: 
             raise RuntimeError('ERROR: [NITAParameters] module not found')
         
         # module MetricsParameters
         if mp is not False:
-            self.vi_change_thresh = float(mp['vi_change_thresh'])
-            self.run_thresh = int(mp['run_thresh'])
-            self.time_step = float(mp['time_step'])
+            self.param_metric = {'vi_change_thresh': float(mp['vi_change_thresh']),
+                                 'run_thresh': int(mp['run_thresh']),
+                                 'time_step': float(mp['time_step'])}
             # TODO: add value check in here
         else:
             raise RuntimeError('ERROR: [MetricsParameters] module not found')
