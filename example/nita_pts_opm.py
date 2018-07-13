68ed778520f5956ee874970361395e7df33b2252
malonzo47 <mikealonzo47@gmail.com>
2018-07-13 11:06:55 -0400

    Updated example script

--- example/nita_pts_opm.py
+++ example/nita_pts_opm.py
@@ -10,6 +10,8 @@ nita = nitaObj(ini)
 # start logging     
 nita.startLog()
 
+nita.loadPts(info_column='Name')
+
 # draw trajectories for selected OBJECTIDs 
 nita.drawPts([1, 2, 4], plot_title=True)
 
@@ -20,7 +22,7 @@ nita.drawPts([9999], plot_title=True)
 nita.setOpmParams()
 
 # set paramters for optmization  with overwrite
-nita.setOpmParams(**{'bail_thresh': [1], 'noise_thresh': [1], 'penalty': [1, 2], 'filt_dist': [3, 5], 'pct': [70], 'max_complex': [10]})
+nita.setOpmParams(**{'bail_thresh_set': [1], 'noise_thresh_set': [1], 'penalty_set': [1, 2], 'filt_dist_set': [3, 5], 'pct_set': [70], 'max_complex_set': [10]})
 
 # parameter optimization 
 nita.paramOpm()
