5e898298300d5ede3c60b7e8c697c1e55feba668
Leyang Feng <feng@american.edu>
2018-07-13 12:00:55 -0400

    Added commandline parser in example scripts.

--- example/nita_pts_opm.py
+++ example/nita_pts_opm.py
@@ -1,8 +1,15 @@
 import numpy as np
 from pynita import *
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-config_file', type=str, help='Full path with file name to INI configuration file.')
+args = parser.parse_args()
+
 # assign the ini file 
-ini = 'user_configs.ini'
+if args.config_file is None:
+    ini = 'user_configs.ini'
+else:
+    ini = args.config_file
 
 # initialize the nita object
 nita = nitaObj(ini)
@@ -16,7 +23,7 @@ nita.loadPts(info_column='Name')
 nita.drawPts([1, 2, 4], plot_title=True)
 
 # draw trajectories for all OBJECTIDs 
-nita.drawPts([9999], plot_title=True)
+#nita.drawPts([9999], plot_title=True)
 
 # set paramters for optmization 
 nita.setOpmParams()
